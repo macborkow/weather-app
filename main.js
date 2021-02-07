6c1b9a1dfe15d4fa9acfd60d00b63673677647c0
macborkow <macborkow@gmail.com>
2021-02-07 21:27:47 +0100

    Add static result

--- main.js
+++ main.js
@@ -10,6 +10,6 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-eval("console.log('kuba to smiec2');\n\nconst fetchWeather = async () => {\n  let result = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=5d5681cfd03e9dfe00bbe5befe716dbc');\n  result = await result.json();\n  console.log(result);\n}\n\nfetchWeather();\n\n//# sourceURL=webpack://my-webpack-project/./src/index.js?");
+eval("let img = document.createElement(\"img\");\ndocument.body.appendChild(img);\n\nconst displayGif = (temp) => {\n  let foo;\n  if (temp < 0) foo = \"cold\";\n  else foo = \"warm\";\n  fetch(\n    \"https://api.giphy.com/v1/gifs/translate?api_key=YgSMbNVTn9H2yFPan735riidNHosuAno&s=\" +\n      foo,\n    { mode: \"cors\" }\n  )\n    .then((response) => {\n      return response.json();\n    })\n    .then((response) => {\n      img.src = response.data.images.original.url;\n    })\n    .catch((e) => {\n      img.src = \"\";\n      throw e;\n    });\n};\n\nconst fetchWeather = async () => {\n  let result = await fetch(\n    \"http://api.openweathermap.org/data/2.5/weather?q=Wroclaw,pl&APPID=5d5681cfd03e9dfe00bbe5befe716dbc\"\n  );\n  result = await result.json();\n  console.log(result);\n  let temp = document.createElement(\"h1\");\n  let tempC = result.main.temp - 273.15;\n  temp.innerText = `Temperature in Wroclaw: ${tempC}`;\n  document.body.appendChild(temp);\n  displayGif(tempC);\n};\n\nfetchWeather();\n\n\n//# sourceURL=webpack://my-webpack-project/./src/index.js?");
 /******/ })()
 ;
