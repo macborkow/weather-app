f124539d8b2e37d1cdd17bf0770263cc5bba808e
macborkow <macborkow@gmail.com>
2021-02-07 11:07:55 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('kuba to smiec2');
+
+const fetchWeather = async () => {
+  let result = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=5d5681cfd03e9dfe00bbe5befe716dbc');
+  result = await result.json();
+  console.log(result);
+}
+
+fetchWeather();
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,13 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+
+
 module.exports = {
+  mode: 'development',
+
   module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-    ],
-  },
-};
+    rules: []
+  }
+}
