696299bb292de67d1bfee6516a7d3dd198485b31
macborkow <macborkow@gmail.com>
2021-02-08 12:59:09 +0100

    Slight style changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+document.body.style.backgroundColor = 'beige';
+
 let wrapper = document.createElement("div");
 wrapper.style.margin = "auto";
 wrapper.style.width = "50vw";
-wrapper.style.top = "15vh";
+wrapper.style.top = "10vh";
 wrapper.style.position = "relative";
 wrapper.style.display = "flex";
 wrapper.style.alignItems = "center";
 wrapper.style.flexDirection = "column";
-wrapper.style.backgroundColor = "gray";
+wrapper.style.backgroundColor = "white";
+wrapper.style.borderRadius = '10vh';
 wrapper.style.padding = "10vh 10vw";
-wrapper.style.height = "50vh";
+wrapper.style.height = "20vh";
+wrapper.style.transition = "0.2s";
 document.body.appendChild(wrapper);
 
 let form = document.createElement("input");
@@ -19,11 +23,15 @@ wrapper.appendChild(form);
 
 let tempDisplay = document.createElement("h1");
 tempDisplay.innerText = "Press enter to search";
+tempDisplay.style.fontSize = '3vh';
 wrapper.appendChild(tempDisplay);
 
 let img = document.createElement("img");
 img.style.height = "35vh";
 img.style.width = "auto";
+img.style.opacity = "0";
+img.style.transition = "0.2s";
+img.style.borderRadius = '10vh';
 wrapper.appendChild(img);
 
 const displayGif = (temp) => {
@@ -40,6 +48,7 @@ const displayGif = (temp) => {
       return response.json();
     })
     .then((response) => {
+      img.style.opacity = "1";
       img.src = response.data.images.original.url;
     })
     .catch((e) => {
@@ -49,7 +58,9 @@ const displayGif = (temp) => {
 };
 
 const fetchWeather = async (city) => {
-  tempDisplay.innerText = "Loading...";
+  wrapper.style.height = "10vh";
+  img.style.opacity = "0";
+  tempDisplay.innerText = "...";
   try {
     let result = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=5d5681cfd03e9dfe00bbe5befe716dbc`
@@ -61,6 +72,7 @@ const fetchWeather = async (city) => {
     tempDisplay.innerText = `Temperature in ${result.name}, ${
       result.sys.country
     }: ${tempC.toFixed(1)}9\xB0C`;
+    wrapper.style.height = "50vh";
   } catch (e) {
     tempDisplay.innerText = `City not found!`;
     img.src = "";
